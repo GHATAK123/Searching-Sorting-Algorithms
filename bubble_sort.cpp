bfb052c72b3e4e28b2bde69cd6b21d56f3d4cb71
PRAKASHANAND <34940322+GHATAK123@users.noreply.github.com>
2019-11-17 23:30:22 +0530

    Add files via upload

---
+++ bubble_sort.cpp
@@ -0,0 +1,35 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+void bubble_sort(int a [], int n)
+{
+	int swapped,i,j;
+	for(i=0;i<n;i++)
+	{
+		swapped=0;
+		for(j=0;j<n-i-1;j++)
+		{
+			if(a[j]>a[j+1])
+			{
+			swap(a[j],a[j+1]);
+			swapped=1;
+			}
+		}
+		if(swapped==0)
+			break;
+		
+	}
+}
+
+int main() {
+	int n;
+	cin >> n;
+	int a[n];
+	for(int k=0;k<n;k++)
+	cin >> a[k];
+	bubble_sort(a,n);
+	for(int l=0;l<n;l++)
+	cout << a[l];
+	
+	return 0;
+}
